5f6ff930412d7638b823f1044a79e36d0a1047de
ErickBautista1706 <99384297+ErickBautista1706@users.noreply.github.com>
2023-04-19 23:01:09 -0600

    Modificacion de diseño de reportes
    Recuerde cambiar cadena de conexion

--- Solucion_web_service_tienda/web_service_tienda/ws/GeneradorReportes.asmx.vb
+++ Solucion_web_service_tienda/web_service_tienda/ws/GeneradorReportes.asmx.vb
@@ -13,49 +13,63 @@ Imports iTextSharp.text.pdf
 Public Class GeneradorReportes
     Inherits System.Web.Services.WebService
 
+
     <WebMethod()>
-    Public Function MostrarVentasPorMes(mesSeleccionado As Integer) As String
-        Dim cadenaConexion As String = "Data Source=LAPTOP-4N86038V; Initial Catalog=tiendaa; User ID= sa; Password=jmsa"
+    Public Sub MostrarVentasPorMes(ByVal mesSeleccionado As Integer)
+        Dim cadenaConexion As String = "Data Source=LAPTOP-I0DKJOIN\ERICKSQLEXPRESS; Initial Catalog=tienda; User ID= sa; Password=1706Erick"
         Dim conexion As New SqlConnection(cadenaConexion)
         Dim comando As New SqlCommand("MostrarVentasPorMes", conexion)
         comando.CommandType = CommandType.StoredProcedure
-
         comando.Parameters.AddWithValue("@Mes", mesSeleccionado)
-
-        conexion.Open()
 
         Dim adaptador As New SqlDataAdapter(comando)
         Dim tablaVentas As New DataTable()
-
         adaptador.Fill(tablaVentas)
-        conexion.Close()
 
-        Dim ventas As String = "Fecha de venta" + " ->                " + "Producto" + " -> " + "Monto" + " -> " + "Precio de venta" + vbCrLf + vbCrLf ' Encabezado
-        For Each fila As DataRow In tablaVentas.Rows
-            ventas += fila("FechaVenta").ToString() + " ->          " + fila("producto").ToString() + " : " + fila("Monto").ToString() + " : " + fila("precioVenta").ToString() + vbCrLf
-        Next
-
-        Return ventas
-    End Function
-
-    <WebMethod()>
-    Public Sub ImprimirTicket(ByVal fechaVenta As Integer)
-        'Generamos el ticket
-        Dim report As String = MostrarVentasPorMes(fechaVenta)
-
-        'Generamos el archivo PDF
         Dim pdfDoc As New iTextSharp.text.Document()
         Dim pdfWriter As PdfWriter = PdfWriter.GetInstance(pdfDoc, HttpContext.Current.Response.OutputStream)
-        pdfDoc.Open()
-        pdfDoc.Add(New Paragraph(report))
-        pdfDoc.Close()
-
-        'Descargamos el archivo PDF
         HttpContext.Current.Response.ContentType = "application/pdf"
-        HttpContext.Current.Response.AddHeader("content-disposition", $"attachment;filename=TICKET-{fechaVenta}.pdf")
+        HttpContext.Current.Response.AddHeader("content-disposition", $"attachment;filename=ReporteVentas-{mesSeleccionado}.pdf")
         HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache)
-        HttpContext.Current.Response.Write(pdfDoc)
+        pdfDoc.Open()
+        pdfDoc.Add(New Paragraph("Reporte de ventas del mes " + mesSeleccionado.ToString() + vbCrLf + vbCrLf))
+
+        Dim fechaActual As Date = Date.MinValue
+        Dim totalMonto As Decimal = 0
+        Dim totalPrecioVenta As Decimal = 0
+
+        For Each fila As DataRow In tablaVentas.Rows
+            Dim fechaVenta As Date = Convert.ToDateTime(fila("FechaVenta"))
+            Dim producto As String = fila("producto").ToString()
+            Dim monto As Decimal = Convert.ToDecimal(fila("Monto"))
+            Dim precioVenta As Decimal = Convert.ToDecimal(fila("precioVenta"))
+
+            If fechaVenta <> fechaActual Then
+                If fechaActual <> Date.MinValue Then
+                    pdfDoc.Add(New Paragraph($"Total monto: {totalMonto}, Total precio venta: {totalPrecioVenta}"))
+                    pdfDoc.Add(New Paragraph(vbCrLf))
+                End If
+
+                pdfDoc.Add(New Paragraph($"Fecha: {fechaVenta.ToString("dd/MM/yyyy")}"))
+                pdfDoc.Add(New Paragraph(vbCrLf))
+
+                fechaActual = fechaVenta
+                totalMonto = 0
+                totalPrecioVenta = 0
+            End If
+
+            pdfDoc.Add(New Paragraph($"{producto} : {monto} : {precioVenta}"))
+
+            totalMonto += monto
+            totalPrecioVenta += precioVenta
+        Next
+
+        pdfDoc.Add(New Paragraph($"Total monto: {totalMonto}, Total precio venta: {totalPrecioVenta}"))
+
+        pdfDoc.Close()
         HttpContext.Current.Response.End()
     End Sub
+
+
 
 End Class
